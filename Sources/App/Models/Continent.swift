7974e20e5b6e76e7af47517ef46191ae521692f4
Mihaela MJ <mihaelamj@me.com>
2018-05-01 17:48:45 +0200

    inserted continents into SQLite

--- Sources/App/Models/Continent.swift
+++ Sources/App/Models/Continent.swift
@@ -3,7 +3,7 @@ import Async
 import Fluent
 import Foundation
 
-public final class Continent<D>: Model where D: QuerySupporting {
+public final class Continent<D>: Model where D: QuerySupporting, D: IndexSupporting {
   
   public typealias Database = D
   
@@ -29,7 +29,7 @@ public final class Continent<D>: Model where D: QuerySupporting {
   }
 }
 
-extension Continent: Migration where D: SchemaSupporting, D: QuerySupporting { }
+extension Continent: Migration where D: SchemaSupporting, D: QuerySupporting, D: IndexSupporting { }
 
 //MARK: - Populating data
 
@@ -44,16 +44,39 @@ let continents : [[String: String]] = [
   ["name": "Nothing", "alpha2": "NN"]
 ]
 
-internal struct ContinentMigration<D>: Migration where D: QuerySupporting & SchemaSupporting {
+internal struct ContinentMigration<D>: Migration where D: QuerySupporting & SchemaSupporting & IndexSupporting {
   typealias Database = D
   
-  static func prepare(on connection: Database.Connection) -> Future<Void> {
+  static func prepareFields(on connection: Database.Connection) -> Future<Void> {
+    return Database.create(Continent<Database>.self, on: connection) { builder in
+      
+      //add fields
+      try builder.field(for: \Continent<Database>.id)
+      try builder.field(for: \Continent<Database>.name)
+      try builder.field(for: \Continent<Database>.alpha2)
+      
+      //indexes
+      try builder.addIndex(to: \.name, isUnique: true)
+    }
+  }
+  
+  static func prepareInsertData(on connection: Database.Connection) ->  Future<Void>   {
     let futures : [EventLoopFuture<Void>] = continents.map { continent in
       let name = continent["name"]!
       let alpha2 = continent["alpha2"]!
       return Continent<D>(name: name, alpha2: alpha2).create(on: connection).map(to: Void.self) { _ in return }
     }
     return Future<Void>.andAll(futures, eventLoop: connection.eventLoop)
+  }
+  
+  static func prepare(on connection: Database.Connection) -> Future<Void> {
+    
+    let futureCreateFields = prepareFields(on: connection)
+    let futureInsertData = prepareInsertData(on: connection)
+    
+    let allFutures : [EventLoopFuture<Void>] = [futureCreateFields, futureInsertData]
+    
+    return Future<Void>.andAll(allFutures, eventLoop: connection.eventLoop)
   }
   
   static func revert(on connection: Database.Connection) -> Future<Void> {
